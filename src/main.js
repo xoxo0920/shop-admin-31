d217f9f7427a740bc35f1efdaf9d0d606b4e38e3
liulong <2721496541@qq.com>
2019-06-06 12:21:18 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router"
 
+// 1.引入element-ui
+import ElementUI from 'element-ui';
+// 2.引入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+//设置全局组件
+Vue.use(ElementUI)
+
+//给原型对象绑定一个新的方法
 Vue.config.productionTip = false
 
 new Vue({
